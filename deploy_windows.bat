c64613ad51f5bb75e68ebfdd069f6452809854d5
nukesaq88 <nukesaq88@gmail.com>
2025-12-28 19:08:12 +0900

    fix win dep

--- deploy_windows.bat
+++ deploy_windows.bat
@@ -2,11 +2,17 @@
 REM Deploy script for Windows distribution
 REM Run this after building in Qt Creator (Release mode)
 
+echo Starting deployment...
+echo.
+
 setlocal
 
 REM Use environment variable if set, otherwise use default
 if not defined BUILD_DIR set BUILD_DIR=build\Desktop_Qt_6_10_1_MinGW_64_bit-Release\release
 set APP_NAME=Pngyu.exe
+
+echo Looking for: %BUILD_DIR%\%APP_NAME%
+echo.
 
 if not exist "%BUILD_DIR%\%APP_NAME%" (
     echo Error: %APP_NAME% not found in %BUILD_DIR%
